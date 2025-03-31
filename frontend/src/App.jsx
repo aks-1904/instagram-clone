3ee47672d4894e0b7c39bf8894bd425f7c32e054
aks-1904 <akshaysharma.work.1904@gmail.com>
2025-03-31 22:33:16 +0530

    creation of login and signup page with api integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import axios from "axios";
+
+const SignUp = () => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const valueChangeHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/user/register",
+        input,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+        setInput({
+          username: "",
+          email: "",
+          password: "",
+        });
+        navigate("/login", {
+          replace: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center w-screen h-screen justify-center px-20 py-5">
+      <form
+        className="lg:w-1/2 shadow-lg rounded-md px-8 py-10 flex flex-col"
+        onSubmit={submitHandler}
+      >
+        <div>
+          <h1 className="text-3xl font-extrabold text-black text-center">
+            LOGO
+          </h1>
+          <p className="text-sm font-semibold text-gray-600 text-center mt-2">
+            Join our community and share your best moments with the world!
+            Connect with friends, explore trending content, and express yourself
+            like never before. Sign up now and start your journey!
+          </p>
+        </div>
+        <div className="mt-5">
+          <div className="my-5">
+            <Label htmlFor={"username"}>Username</Label>
+            <Input
+              type={"text"}
+              id={"username"}
+              className={"focus-visible:ring-transparent"}
+              name={"username"}
+              value={input.username}
+              onChange={valueChangeHandler}
+            />
+          </div>
+          <div className="my-5">
+            <Label htmlFor={"email"}>E-mail</Label>
+            <Input
+              type={"email"}
+              id={"email"}
+              className={"focus-visible:ring-transparent"}
+              name={"email"}
+              value={input.email}
+              onChange={valueChangeHandler}
+            />
+          </div>
+          <div className="my-5">
+            <Label htmlFor={"password"}>Password</Label>
+            <Input
+              type={"password"}
+              id={"password"}
+              className={"focus-visible:ring-transparent"}
+              name={"password"}
+              value={input.password}
+              onChange={valueChangeHandler}
+            />
+          </div>
+          <div className="-mt-5 mb-5 text-end">
+            Already have an account?{" "}
+            <Link
+              to={"/login"}
+              replace={true}
+              className="text-blue-500 underline cursor-pointer"
+            >
+              Login
+            </Link>
+          </div>
+          {loading ? (
+            <Button disabled className={"cursor-pointer w-full"}>
+              Please Wait...
+            </Button>
+          ) : (
+            <Button type={"submit"} className={"cursor-pointer w-full"}>
+              Register
+            </Button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
